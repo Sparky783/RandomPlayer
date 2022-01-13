2dd54244b6df4263c242efa4738ca008b63c3546
Florent Lavignotte <florent.lavignotte@gmail.com>
2022-01-13 10:11:20 +0100

    Final version of RandomFile project. That project was the first part of the new project PrivatePlayer

---
+++ RandomFile/Models/FileType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomFile.Models
+{
+    public enum FileType
+    {
+        None,
+        Movie,
+        Picture,
+        Song
+    }
+}
